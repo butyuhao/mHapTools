3e82c2ca49824b582a12be2a3757fae2ba5913b3
butyuhao <butyuhao@hotmail.com>
2020-04-13 20:25:08 +0800

    add htslib

---
+++ front_end.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <htslib/sam.h>
+using namespace std;
+int main(int argc, char *argv[]) {
+//  if (argc < 2) {
+//    cout << "See usage" << endl;
+//  }
+//  int ret = 0;
+//  if (strcmp(argv[1], ""))
+
+  return 0;
+}
