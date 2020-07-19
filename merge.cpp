b0be31c607f1eee7c3689a30c67ea0c5e5d1bdb5
butyuhao <butyuhao@hotmail.com>
2020-07-19 09:15:23 +0800

    add get_cpg()

--- merge.cpp
+++ merge.cpp
@@ -7,8 +7,40 @@
 #include "hap.cpp"
 #include "merge.h"
 
-
 namespace std {
+
+extern int _lower_bound(vector<hts_pos_t> &v, hts_pos_t &cpg_pos);
+
+vector<hap_pos_t> get_cpg(ContextMerge &ctx_merge, hap_t &hap_read) {
+  /*
+   * get cpg pos for a hap read and store to hap_t
+   */
+  vector<hap_pos_t>_cpg_pos(hap_read.hap_str.size(), 0);
+
+  int pos = _lower_bound(ctx_merge.cpg_pos_map[hap_read.chr], hap_read.chr_beg);
+
+  unordered_map<string, vector<hts_pos_t>>::iterator cpg_pos_map_itor;
+
+  cpg_pos_map_itor = ctx_merge.cpg_pos_map.find(hap_read.chr);
+
+  int i = 0;
+  if (cpg_pos_map_itor != ctx_merge.cpg_pos_map.end()) {
+    while (pos < ctx_merge.cpg_pos_map[hap_read.chr].size()) {
+
+      if (ctx_merge.cpg_pos_map[hap_read.chr][pos] >= hap_read.chr_beg &&
+      ctx_merge.cpg_pos_map[hap_read.chr][pos] <= hap_read.chr_end) {
+
+        _cpg_pos[i] = ctx_merge.cpg_pos_map[hap_read.chr][pos];
+        i++;
+        pos++;
+      } else {
+        break;
+      }
+    }
+  }
+
+  return _cpg_pos;
+}
 
 bool load_chr_cpg(ContextMerge &ctx_merge) {
   /*
@@ -102,6 +134,7 @@ bool load_chr_cpg(ContextMerge &ctx_merge) {
 
   if (!opt_check(ctx_merge)) {
     hts_log_error("opt error");
+    return 1;
   }
 
   load_chr_cpg(ctx_merge);
@@ -115,11 +148,13 @@ bool load_chr_cpg(ContextMerge &ctx_merge) {
 
 
   while(hap_read(fp_hap1, &hap_t_1) == 0 && hap_read(fp_hap2, &hap_t_2) == 0) {
+    vector<hap_pos_t> cpg_pos_1;
+    vector<hap_pos_t> cpg_pos_2;
+
+    cpg_pos_1 = get_cpg(ctx_merge, hap_t_1);
+    cpg_pos_2 = get_cpg(ctx_merge, hap_t_2);
 
   }
-
-
-
 
   return 0;
 }
